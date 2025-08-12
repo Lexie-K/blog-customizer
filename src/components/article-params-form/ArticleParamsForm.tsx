4331a2ae535c41c77852518c8832a6baf3e62771
Lexie-K <xandra.kk@yandex.ru>
2025-08-13 00:54:08 +0300

    edited

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Button } from 'src/ui/button';
+import { ArrowButton } from 'src/ui/arrow-button';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Select } from 'src/ui/select';
 import styles from './ArticleParamsForm.module.scss';
@@ -39,6 +40,42 @@ export const ArticleParamsForm: React.FC<IArticleParamsForm> = ({
 		initialSettings.contentWidth
 	);
 
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	const sidebarRef = useRef<HTMLFormElement>(null);
+
+	useEffect(() => {
+		if (!isSidebarOpen) {
+			return;
+		}
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsSidebarOpen(false);
+			}
+		};
+
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setIsSidebarOpen(false);
+			}
+		};
+
+		window.addEventListener('mousedown', handleClickOutside);
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('mousedown', handleClickOutside);
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [isSidebarOpen]);
+	const toggleSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+
 	useEffect(() => {
 		setCurrentFontFamily(initialSettings.fontFamilyOption);
 		setCurrentFontSize(initialSettings.fontSizeOption);
@@ -59,72 +96,79 @@ export const ArticleParamsForm: React.FC<IArticleParamsForm> = ({
 		onApply(newAppliedSettings);
 	};
 
-	const handleResetClick = () => {
+	const handleReset = () => {
 		onReset();
 	};
 
 	return (
-		<form className={styles.form} onSubmit={handleSubmit}>
-			<h2 className={styles.sidebarTitle}>Задайте параметры</h2>
-			<div className={styles.paramGroup}>
-				<Select
-					title='Шрифт'
-					options={fontFamilyOptions}
-					selected={currentFontFamily}
-					onChange={setCurrentFontFamily}
-					placeholder='Выберите шрифт'
-				/>
-			</div>
+		<>
+			<ArrowButton onClick={toggleSidebar} isOpen={isSidebarOpen} />
+			<aside
+				className={`${styles.container} ${
+					isSidebarOpen ? styles.container_open : ''
+				}`}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}
+					ref={sidebarRef}>
+					<h2 className={styles.sidebarTitle}>Задайте параметры</h2>
+					<div className={styles.paramGroup}>
+						<Select
+							title='Шрифт'
+							options={fontFamilyOptions}
+							selected={currentFontFamily}
+							onChange={setCurrentFontFamily}
+							placeholder='Выберите шрифт'
+						/>
+					</div>
 
-			<div className={styles.paramGroup}>
-				<RadioGroup
-					title='Размер шрифта'
-					name='fontSize'
-					options={fontSizeOptions}
-					selected={currentFontSize}
-					onChange={setCurrentFontSize}
-				/>
-			</div>
+					<div className={styles.paramGroup}>
+						<RadioGroup
+							title='Размер шрифта'
+							name='fontSize'
+							options={fontSizeOptions}
+							selected={currentFontSize}
+							onChange={setCurrentFontSize}
+						/>
+					</div>
 
-			<div className={styles.paramGroup}>
-				<Select
-					title='Цвет шрифта'
-					options={fontColors}
-					selected={currentFontColor}
-					onChange={setCurrentFontColor}
-					placeholder='Выберите цвет шрифта'
-				/>
-			</div>
+					<div className={styles.paramGroup}>
+						<Select
+							title='Цвет шрифта'
+							options={fontColors}
+							selected={currentFontColor}
+							onChange={setCurrentFontColor}
+							placeholder='Выберите цвет шрифта'
+						/>
+					</div>
 
-			<div className={styles.paramGroup}>
-				<Select
-					title='Цвет фона'
-					options={backgroundColors}
-					selected={currentBackgroundColor}
-					onChange={setCurrentBackgroundColor}
-					placeholder='Выберите цвет фона'
-				/>
-			</div>
+					<div className={styles.paramGroup}>
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={currentBackgroundColor}
+							onChange={setCurrentBackgroundColor}
+							placeholder='Выберите цвет фона'
+						/>
+					</div>
 
-			<div className={styles.paramGroup}>
-				<Select
-					title='Ширина контента'
-					options={contentWidthArr}
-					selected={currentContentWidth}
-					onChange={setCurrentContentWidth}
-					placeholder='Выберите ширину контента'
-				/>
-			</div>
+					<div className={styles.paramGroup}>
+						<Select
+							title='Ширина контента'
+							options={contentWidthArr}
+							selected={currentContentWidth}
+							onChange={setCurrentContentWidth}
+							placeholder='Выберите ширину контента'
+						/>
+					</div>
 
-			<div className={styles.bottomContainer}>
-				<Button
-					title='Сбросить'
-					htmlType='reset'
-					type='clear'
-					onClick={handleResetClick}
-				/>
-				<Button title='Применить' htmlType='submit' type='apply' />
-			</div>
-		</form>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-import { ArrowButton } from './ui/arrow-button';
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-import { SideBar } from './components/sidebar';
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+import { App } from './App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-	const [articleStyles, setArticleStyles] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	const toggleSidebar = () => {
-		setIsSidebarOpen(true);
-	};
-
-	const closeSidebar = () => {
-		setIsSidebarOpen(false);
-	};
-
-	const handleApplyArticleParams = (newSettings: ArticleStateType) => {
-		setArticleStyles(newSettings);
-	};
-
-	const handleResetArticleParams = () => {
-		setArticleStyles(defaultArticleState);
-	};
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': articleStyles.fontFamilyOption.value,
-					'--font-size': articleStyles.fontSizeOption.value,
-					'--font-color': articleStyles.fontColor.value,
-					'--container-width': articleStyles.contentWidth.value,
-					'--bg-color': articleStyles.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArrowButton onClick={toggleSidebar} isOpen={isSidebarOpen} />
-			<SideBar isOpen={isSidebarOpen} onClose={closeSidebar}>
-				<ArticleParamsForm
-					initialSettings={articleStyles}
-					onApply={handleApplyArticleParams}
-					onReset={handleResetArticleParams}
-				/>
-			</SideBar>
-			<Article currentArticleStyles={articleStyles} />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
